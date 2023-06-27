cb6c4e9c1cde5ddf64e596b540d127e5d90ec283
alek <wait82985@gmail.com>
2023-06-27 17:29:44 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,21 @@
 public class Solution {
     public int maxPathSum(TreeNode root) {
-        //TODO
-        return 0;
+        if (root.left == null && root.right == null) return root.val;
+        int[] maxSum = new int[1];
+        maxSum[0] = Integer.MIN_VALUE;
+        dfs(root, maxSum);
+        return maxSum[0];
+    }
+
+    private int dfs(TreeNode root, int[] maxSum) {
+        if (root == null) return 0;
+
+        int leftSum = Math.max(0, dfs(root.left, maxSum));
+        int rightSum = Math.max(0, dfs(root.right, maxSum));
+
+        maxSum[0] = Math.max(maxSum[0], leftSum + rightSum + root.val);
+
+
+        return Math.max(leftSum, rightSum) + root.val;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -25,5 +25,23 @@ public class SolutionTest {
 
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void maxPathSumTest3() {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.left = new TreeNode(1);
+
+        int expected = 48;
+        int actual = new Solution().maxPathSum(root);
+
+        Assert.assertEquals(expected, actual);
+    }
+
 
 }
